dc2ae82ad82890aa911b6b53da49890a3249145e
lau1406 <lkeijzer@live.nl>
2016-12-12 17:08:47 +0100

    added psvm

--- src/main.java
+++ src/main.java
@@ -2,4 +2,13 @@
  * Created by Laurence on 2016-12-12.
  */
 public class main {
+
+    public main() {
+        // TODO: init code here
+    }
+
+
+    public static void main(String[] args) {
+        new main();
+    }
 }
