1d38a5999fb69c834dcaae377fb7d80f3a67f34b
lau1406 <lkeijzer@live.nl>
2016-12-14 00:20:49 +0100

    added brute force approach

--- src/HW2/Exercise1c.java
+++ src/HW2/Exercise1c.java
@@ -5,18 +5,59 @@ import data.Filter;
 import data.Record;
 import data.RecordSet;
 
+import java.util.*;
+
 /**
  *
  * @author sjef
  */
 public class Exercise1c extends Exercise  {
+    private ArrayList<Float> sensitivities;
 
     @Override
     public void run() {
         System.out.println("Exercise 1c");
 
-        float sensitivity = genSensitivity(recordSet, "samerace", "1");
-        System.out.println("sensitivity = " + sensitivity);
+        // Brute force
+        sensitivities = new ArrayList<>();
+        float highestSansitivity = 0;
+        String highestAttribute = "";
+        String highestValue = "";
+
+        ArrayList<String> attr = new ArrayList<>();
+        attr.addAll(recordSet.getAttributeValues().keySet());
+        ArrayList<String> values = new ArrayList<>();
+
+
+        for (String s : attr) {
+            // attribute 'match' gives 1 for obvious reasons, we skip this one
+            if (s.equals("match")) continue;
+            // Loop over all attributes
+            values.clear();
+            values.addAll(recordSet.getAttributeValues().get(s));
+            for (String value : values) {
+                // Loop over all possible values
+                float sensitivity = genSensitivity(recordSet, s, value);
+                sensitivities.add(sensitivity);
+                if (sensitivity >= highestSansitivity) {
+                    highestSansitivity = sensitivity;
+                    highestAttribute = s;
+                    highestValue = value;
+                }
+            }
+        }
+
+        // Not rly useful without a mapping to the attributes and values
+        sensitivities.sort((o1, o2) -> {
+            if (o1 > o2) return -1;
+            if (o1 < o2) return 1;
+            return 0;
+        });
+
+        System.out.println("Highest attribute value combo: " + highestAttribute + ":" + highestValue);
+        System.out.println("With a sensitivity of: " + highestSansitivity);
+        System.out.println("From all these values:");
+        System.out.println(sensitivities);
     }
 
     private float genSensitivity(RecordSet recordSet, String attr, String attrValue) {
