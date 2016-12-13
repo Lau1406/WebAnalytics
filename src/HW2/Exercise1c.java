2b6786404348909e3c9e415c762ea35ba5bb9e3f
lau1406 <lkeijzer@live.nl>
2016-12-13 22:27:47 +0100

    initial code to gen sensitivity of 1 arrtibute

--- src/HW2/Exercise1c.java
+++ src/HW2/Exercise1c.java
@@ -1,6 +1,10 @@
 package HW2;
 
 
+import data.Filter;
+import data.Record;
+import data.RecordSet;
+
 /**
  *
  * @author sjef
@@ -10,6 +14,28 @@ public class Exercise1c extends Exercise  {
     @Override
     public void run() {
         System.out.println("Exercise 1c");
+
+        float sensitivity = genSensitivity(recordSet, "samerace", "1");
+        System.out.println("sensitivity = " + sensitivity);
+    }
+
+    private float genSensitivity(RecordSet recordSet, String attr, String attrValue) {
+        // Get big P
+        int bigP = RecordSet.filter(recordSet, new Filter() {
+            @Override
+            public boolean filterRecord(Record record) {
+                return record.get("match").equals("1");
+            }
+        }).getRecords().size();
+
+        // Gen small p based on attr
+        int smallP = RecordSet.filter(recordSet, new Filter() {
+            @Override
+            public boolean filterRecord(Record record) {
+                return record.get(attr).equals(attrValue) && record.get("match").equals("1");
+            }
+        }).getRecords().size();
+        return (float)smallP / bigP;
     }
     
 }
