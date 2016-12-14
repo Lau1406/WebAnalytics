c3b63ed84da3b866c055dd1acbb58085601b410f
JochemKuijpers <github.com@jochemkuijpers.nl>
2016-12-14 23:51:32 +0100

    Remove unused filters in exercise 1b

--- src/HW2/Exercise1b.java
+++ src/HW2/Exercise1b.java
@@ -10,13 +10,6 @@ public class Exercise1b extends Exercise  {
 
     @Override
     public void run() {
-        RecordSet filteredSet = RecordSet.filter(recordSet, new Filter() {
-            @Override
-            public boolean filterRecord(Record record) {
-                return record.get("ambtition_important").equals("6.67") && record.get("match").equals("1") ;
-            }
-        });
-        //System.out.println("Only " + filteredSet.getRecords().size() + " records have gaming=10");
 
         Specificity qm = new Specificity(recordSet, new MatchFilter());
         FindSubGroup fs = new FindSubGroup(recordSet, qm, 1);
