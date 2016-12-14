5ef4deb7c467ab12695cc513ddf26dbfbb7dc2a3
JochemKuijpers <github.com@jochemkuijpers.nl>
2016-12-14 23:51:04 +0100

    Remove unused filters in exercise 1a

--- src/HW2/Exercise1a.java
+++ src/HW2/Exercise1a.java
@@ -1,9 +1,6 @@
 package HW2;
 
-import data.Filter;
 import data.FindSubGroup;
-import data.Record;
-import data.RecordSet;
 import data.WeightedRelativeAccuracy;
 
 /**
@@ -15,39 +12,7 @@ public class Exercise1a extends Exercise {
 
     @Override
     public void run() {
-        Filter attr_like = new Filter() {
-            @Override
-            public boolean filterRecord(Record record) {
-                return record.get("d_attractive_o").equals("[6-8]") && record.get("d_like").equals("[6-8]");
-            }
-        };
-        Filter best = new Filter() {
-            @Override
-            public boolean filterRecord(Record record) {
-                return record.get("match").equals("1");
-            }
-        };
-        Filter prob = new Filter(){
-            @Override
-            public boolean filterRecord(Record record) {
-                return record.get("d_guess_prob_liked").equals("[7-10]");
-            }
-        };
-        Filter sharedint = new Filter() {
 
-            @Override
-            public boolean filterRecord(Record record) {
-                return record.get("d_shared_interests_o").equals("[6-8]");
-            }
-        };
-        Filter sharedintpar = new Filter() {
-
-            @Override
-            public boolean filterRecord(Record record) {
-                return record.get("d_shared_interests_partner").equals("[6-8]");
-            }
-        };
-        
         WeightedRelativeAccuracy qm = new WeightedRelativeAccuracy(recordSet, new MatchFilter());
         FindSubGroup fs = new FindSubGroup(recordSet, qm, 0);
         fs.excludeAttribute("match");
